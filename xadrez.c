3c3aa1541a26af9d7ff812bb5972a77a827d515b
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-02-10 22:01:49 +0000

    Adicionado loop de movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -7,7 +7,7 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int bispo = 5, torre = 5, rainha = 8;
+    int bispo = 5, torre = 5, rainha = 8, cavalo = 2;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
@@ -35,6 +35,13 @@ int main() {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("\nMovimentação do cavalo:\n");
+    do {
+        for (cavalo; cavalo != 0; cavalo --) {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    } while(0);
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
